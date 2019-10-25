6e4a58700372ccd4f72d8de6f3905b99e585a1d0
svergin <svergin@EN4165465-222.fulton.ad.asu.edu>
2019-10-25 14:07:36 -0700

    changed speed on manual controls

--- ManualControls.m
+++ ManualControls.m
@@ -2,20 +2,20 @@ global key
 InitKeyboard();
 
 while 1
-    pause(0.2)
+    pause(0.1)
     switch key
         case 'uparrow'
-            brick.MoveMotor('A', 100);
-            brick.MoveMotor('B', 100);
+            brick.MoveMotor('A', 49);
+            brick.MoveMotor('B', 50);
         case 'downarrow'
-            brick.MoveMotor('A', -100);
-            brick.MoveMotor('B', -100);
+            brick.MoveMotor('A', -50);
+            brick.MoveMotor('B', -50);
         case 'leftarrow'
-            brick.MoveMotor('A', -100);
-            brick.MoveMotor('B', 100);
+            brick.MoveMotor('A', -50);
+            brick.MoveMotor('B', 50);
         case 'rightarrow'
-            brick.MoveMotor('A', 100);
-            brick.MoveMotor('B', -100);
+            brick.MoveMotor('A', 50);
+            brick.MoveMotor('B', -50);
         case 0
             brick.StopMotor('A');
             brick.StopMotor('B');
