aa24effdbbbbc67afa04431a9e2a2ae767ae0537
svergin <svergin@EN4165465-222.fulton.ad.asu.edu>
2019-11-01 14:32:19 -0700

    hi

--- BeepSwitch.m
+++
@@ -1,8 +0,0 @@
-disp('PUSH button to start the tone')
-while brick.TouchPressed(1)~=0
-    brick.playTone(100,300,500)
-    disp('RELEASE button to turn tone OFF.')
-    pause(0.5)
-end
-
-disp('done!')
--- Beeper.m
+++
@@ -1,8 +0,0 @@
-display('Push the button.')
-
-while brick.TouchPressed(1)==0
-    brick.playTone(100,300,500);
-    pause(0.75);
-end
-
-display('done!')
