7e3019f19d6c2f791e2815e7e4542e4bd1d94e94
svergin <svergin@EN4165465-222.fulton.ad.asu.edu>
2019-11-01 14:30:58 -0700

    added autonomous class, color sensor manual code, updated manual, ultrasonic sensor manual code

--- ManualControls.m
+++ ManualControls.m
@@ -5,20 +5,27 @@ while 1
     pause(0.1)
     switch key
         case 'uparrow'
-            brick.MoveMotor('A', 49);
-            brick.MoveMotor('B', 50);
+            brick.MoveMotor('A', 16);
+            brick.MoveMotor('B', 16);
         case 'downarrow'
-            brick.MoveMotor('A', -50);
-            brick.MoveMotor('B', -50);
-        case 'leftarrow'
-            brick.MoveMotor('A', -50);
-            brick.MoveMotor('B', 50);
+            brick.MoveMotor('A', -16);
+            brick.MoveMotor('B', -16);
         case 'rightarrow'
-            brick.MoveMotor('A', 50);
-            brick.MoveMotor('B', -50);
+            brick.MoveMotor('A', -16);
+            brick.MoveMotor('B', 16);
+        case 'leftarrow'
+            brick.MoveMotor('A', 16);
+            brick.MoveMotor('B', -16);
+        case 's'
+            brick.MoveMotor('C', 3);
+            
+        case 'w'
+            brick.MoveMotor('C', -3);
+            
         case 0
             brick.StopMotor('A');
             brick.StopMotor('B');
+            brick.StopMotor('C');
         case 'q'
             break;
     end
